99dcd03bb9c1ddf61f382480d1051f76267ff5d3
Mehadi Athani <mehadi.a@fireflink.com>
2023-04-24 15:08:13 +0530

    my new prj

---
+++ src/test/java/com/git/pract/Sweet/SweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.pract.Sweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
